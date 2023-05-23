29db8a85380160aff04c92a54b2049519c86d572
Deniz <77787415+deniz244@users.noreply.github.com>
2023-05-23 15:20:13 +0300

    sorular index.js içinde çözüldü

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:
    3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function carpma(a,b) {
+  return a*b;
 }
 
 /* Görev 2 : Köpeğin Yaşı */
@@ -65,8 +65,8 @@ Aşağıdakileri yap:
  3. Hesaplanan köpeğin yaşını dönün.
  */
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function kopeginYasi(x) {
+  return x*7;
 }
 
 /* Görev 3 */
@@ -84,7 +84,32 @@ OYUNUN KURALLARI: Makas Kağıdı yener| Kağıt Taşı yener | Taş Makas'ı ye
 */
 
 function oyun(oyuncu, bilgisayar) {
-  /*buraya kodunu yazabilirsin*/
+   if(oyuncu=='Taş' && bilgisayar == 'Makas'){
+    return "Kazandın!" 
+   }
+   else if(oyuncu=='Makas' && bilgisayar == 'Kağıt'){
+    return "Kazandın!" 
+   }
+
+   else if(oyuncu=='Kağıt' && bilgisayar == 'Taş'){
+    return "Kazandın!" 
+   }
+
+   else if(oyuncu=='Taş' && bilgisayar == 'Taş'){
+    return "Beraberlik" 
+   }
+   else if(oyuncu=='Makas' && bilgisayar == 'Makas'){
+    return "Beraberlik" 
+   }
+
+   else if(oyuncu=='Kağıt' && bilgisayar == 'Kağıt'){
+    return "Beraberlik" 
+   }
+
+   else{
+    return "Kaybettin!"
+   }
+
 }
 
 // Şimdi Taş, Kağıt, Makas oyununu bilgisayara karşı oynayalım!
@@ -113,8 +138,8 @@ Aşağdaki milDonusturucu fonksiyonunu aşağıdakileri kullanarak tamamla:
 3. Mil değerini geri dönün
 */
 
-function milDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function milDonusturucu(km) {
+  return km * 0.621371;
 }
 
 //Görev 4b - Santimetreden Feet
@@ -127,8 +152,8 @@ Aşağıdakileri feetDonusturucu fonsiyonunu kullanarak yap:
 Google'da arama ipucu: "feet cm dönüştürme"
 */
 
-function feetDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function feetDonusturucu(cm) {
+  return cm / 30.48; 
 }
 
 /* Görev 5 : 5 küçük maymun yatakta zıplamış şarkısını çocuklar için hazırladığımı varsayalım. https://www.youtube.com/watch?v=e4EJ34xnlxk */
@@ -144,8 +169,15 @@ Aşağıdakileri cocukSarkisi fonksiyonunda yap:
 4. Bu döngüde, her seferinde cocukSarkisi fonsiyonu çalışsın ve console.log'a dönen metni yazdırsın.
 */
 
-function cocukSarkisi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function cocukSarkisi(n) {
+
+  let i=n;
+  
+  while(i!=1){
+    i--
+  }
+
+  return (n + " küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!")
 }
 
 /* Görev 6 : Not Hesaplayıcı */
@@ -163,8 +195,23 @@ Aşağdakileri notHesapla fonksiyonunda yap.
  dönün
 */
 
-function notHesapla(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function notHesapla(not) {
+  if(not>=90 && not<=100 ){
+    return "A aldın"
+  }
+  else if( not >= 80 && not<=89){
+    return "B aldın"
+  }
+  else if( not >= 70 && not<=79){
+    return "C aldın"
+  }
+  else if( not >= 60 && not<=69){
+    return "D aldın"
+  }
+  else{
+    return "F aldın"
+  }
+
 }
 
 /* Bonus Çalışma: Sesli harf sayacı - Kaç tane sesli harf var? */
